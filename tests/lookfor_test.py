5996538c1d69e3fcde134bf0c4d9201a6b22a647
breakthatbass <gamache.taylor@gmail.com>
2020-07-03 10:32:44 -0400

    add test

--- tests/lookfor_test.py
+++ tests/lookfor_test.py
@@ -2,19 +2,29 @@ import pytest
 import sys
 
 sys.path.append("../lookfor")
-from lookfor import find_dir, find_ext_all
+from lookfor import find_dir, find_ext_all, find_ext_in_dir
 
 
 @pytest.mark.parametrize('test_input, expected', [
     ('test_dir', './test_dir'),  
     ('photos', None),
-    ('test_dir_two', './test_dir/test_dir_two')])
+    ('test_dir_two', './test_dir/test_dir_two')
+])
 def test_find_dir(test_input, expected):
     assert find_dir(test_input) == expected
 
 
 @pytest.mark.parametrize('test_input, expected', [
     ('.c', 'found 0 .c files'),  
-    ('.txt', 'found 3 .txt files')])
+    ('.txt', 'found 3 .txt files')
+])
 def test_find_ext_all(test_input, expected):
     assert find_ext_all(test_input) == expected
+
+
+@pytest.mark.parametrize('test_input, expected', [
+    ('.c', 'found 0 .c files'),
+    ('txt', 'found 1 .txt files')
+])
+def test_find_ext_in_dir(test_input, expected):
+    assert find_ext_in_dir(test_input) == expected
