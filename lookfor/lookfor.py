094633bebbbd65edd80147653f56c836baa45ad8
breakthatbass <gamache.taylor@gmail.com>
2020-07-03 10:23:12 -0400

    add find_ext_all test

--- lookfor/lookfor.py
+++ lookfor/lookfor.py
@@ -7,3 +7,8 @@ def find_dir(dir):
             if name == dir:
                 return os.path.join(root, name)
                 # if not found, it returns None
+
+
+def find_ext_all(ext):
+    '''searches for all files with ext in all directories starting with current directory'''
+    pass
--- tests/lookfor_test.py
+++ tests/lookfor_test.py
@@ -2,7 +2,7 @@ import pytest
 import sys
 
 sys.path.append("../lookfor")
-from lookfor import find_dir
+from lookfor import find_dir, find_ext_all
 
 
 @pytest.mark.parametrize('test_input, expected', [
@@ -11,3 +11,10 @@ from lookfor import find_dir
     ('test_dir_two', './test_dir/test_dir_two')])
 def test_find_dir(test_input, expected):
     assert find_dir(test_input) == expected
+
+
+@pytest.mark.parametrize('test_input, expected', [
+    ('.c', 'found 0 .c files'),  
+    ('.txt', 'found 3 .txt files')])
+def test_find_ext_all(test_input, expected):
+    assert find_ext_all(test_input) == expected
