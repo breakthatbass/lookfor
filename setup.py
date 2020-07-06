35b288ae580483a96049b5ceb5649217f2275958
breakthatbass <gamache.taylor@gmail.com>
2020-07-06 15:32:04 -0400

    update setup.py file

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='lookfor',
     version='1.0.0',
+    description='terminal program to look for things',
     author='Taylor Gamache',
+    author_email='gamache.taylor@gmail.com',
+    url='https://github.com/breakthatbass/lookfor',
+    packages=find_packages(),
+    license='MIT',
     entry_points={
         'console_scripts' : ['lookfor = lookfor.lookfor:main']
    }
