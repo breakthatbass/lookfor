3e2f3b610dd61fc1488b1982684f557de84aef00
breakthatbass <gamache.taylor@gmail.com>
2020-08-20 22:42:20 -0400

    update version in __init__ file

---
+++ lookfor/__init__.py
@@ -0,0 +1,13 @@
+'''
+lookfor
+
+Lookfor looks for things: files, directories, files by extension, and strings within files.
+Also, replace strings in files with new strings.
+
+'''
+
+__version__ = '1.0.0'
+__author__ = 'Taylor Gamache'
+
+
+
